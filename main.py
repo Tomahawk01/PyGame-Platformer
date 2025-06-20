d63c90e54e7802cfad286ea6e1bc0eb5f81d1a32
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-06-19 17:25:40 -0700

    Basic player setup
    - Added PhysicsEntity class
    - load_image() utility

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 import sys
 import pygame
 
-pygame.init()
-pygame.display.set_caption("Platformer Game")
+from src.utils import load_image
+from src.entities import PhysicsEntity
 
-window = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Platformer Game")
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.window = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
-    pygame.display.update()
-    clock.tick(60)
+        self.clock = pygame.time.Clock()
+
+        self.movement = [False, False]
+
+        self.assets = {
+            "player": load_image("entities/player.png"),
+        }
+
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+
+    def run(self):
+        while True:
+            self.display.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+            self.window.blit(pygame.transform.scale(self.display, self.window.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()

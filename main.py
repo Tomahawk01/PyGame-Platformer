4c75fc4f291339c8aad38727c400c28c24db8fb1
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-06-23 23:40:03 -0700

    Enemies shooting projectiles

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class Game:
             "particle/leaf": Animation(load_images("particles/leaf"), 20, False),
             "particle/particle": Animation(load_images("particles/particle"), 6, False),
             "gun": load_image("gun.png"),
+            "projectile": load_image("projectile.png"),
         }
 
         self.clouds = Clouds(self.assets["clouds"], 16)
@@ -60,6 +61,7 @@ class Game:
             else:
                 self.enemies.append(Enemy(self, spawner["pos"], (8, 15)))
         
+        self.projectiles = []
         self.particles = []
 
         self.scroll = [0, 0]
@@ -88,6 +90,19 @@ class Game:
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display, render_scroll)
+
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1]
+                projectile[2] += 1
+                img = self.assets["projectile"]
+                self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1]))
+                if self.tilemap.solid_check(projectile[0]):
+                    self.projectiles.remove(projectile)
+                elif projectile[2] > 360:
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50:
+                    if self.player.rect().collidepoint(projectile[0]):
+                       self.projectiles.remove(projectile) 
 
             for particle in self.particles.copy():
                 kill = particle.update()

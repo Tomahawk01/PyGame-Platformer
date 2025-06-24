fac3924e2a90c4db6945624b777ab209ed821804
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-06-24 09:53:52 -0700

    Added screenshake

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ class Game:
 
         self.load_level(0)
 
+        self.screenshake = 0
+
     def load_level(self, map_id):
         self.tilemap.load("data/maps/" + str(map_id) + ".json")
 
@@ -77,6 +79,8 @@ class Game:
     def run(self):
         while True:
             self.display.blit(self.assets["background"], (0, 0))
+
+            self.screenshake = max(0, self.screenshake - 1)
 
             if self.dead:
                 self.dead += 1
@@ -122,6 +126,7 @@ class Game:
                     if self.player.rect().collidepoint(projectile[0]):
                        self.projectiles.remove(projectile)
                        self.dead += 1
+                       self.screenshake = max(16, self.screenshake)
                        for i in range(30):
                            angle = random.random() * math.pi * 2
                            speed = random.random() * 5
@@ -161,7 +166,8 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
-            self.window.blit(pygame.transform.scale(self.display, self.window.get_size()), (0, 0))
+            screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
+            self.window.blit(pygame.transform.scale(self.display, self.window.get_size()), screenshake_offset)
             pygame.display.update()
             self.clock.tick(60)
 
